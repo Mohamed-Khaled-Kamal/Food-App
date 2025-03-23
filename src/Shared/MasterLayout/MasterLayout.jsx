21013021b2f56830433a22bad9cb0810dec00fa8
Mohamed-Khaled-Kamal <mo7med.khaled22@gmail.com>
2025-03-23 17:32:10 +0200

    User and Admin Complete

--- src/Shared/MasterLayout/MasterLayout.jsx
+++ src/Shared/MasterLayout/MasterLayout.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
-import SideBaar from '../SideBar/SideBar'
+import Sidemenu from '../SideBar/Sidemenu'
+
 
 export default function MasterLayout({loginData}) {
   return (
     <div>
         <div className="d-flex ">
               <div className=''>
-                  <SideBaar loginData={loginData}/>
+                  <Sidemenu loginData={loginData}/>
               </div>
               <div className='w-100 vh-100 overflow-auto'>
                   <Navbar loginData={loginData} />
